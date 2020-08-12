b2bf443d8f94c7165a0d475ae6746f335f42d584
farhat97 <44099258+farhat97@users.noreply.github.com>
2020-08-11 16:51:24 -0800

    Fixed table data formatting

--- src/app/covid-tracking-project/covid-tracking-project.component.ts
+++ src/app/covid-tracking-project/covid-tracking-project.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CovidTrackingProjectService } from '../shared/covid-tracking-project.service';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-covid-tracking-project',
@@ -35,27 +36,45 @@ export class CovidTrackingProjectComponent implements OnInit {
     let covidApiResults = { };
 
     // add desired fields to covidApiResults object
-    covidApiResults["Date"] = resultObj["date"];
+    covidApiResults["Date"] = this.formatDate(resultObj["date"]);
     covidApiResults["Tracked States (or territories)"] = resultObj["states"];
-    covidApiResults["Positive Cases"] = resultObj["positive"];
-    covidApiResults["Positive Cases Increase"] = resultObj["positiveIncrease"];
-    covidApiResults["Negative Cases"] = resultObj["negative"];
-    covidApiResults["Negative Cases Increase"] = resultObj["negativeIncrease"];
-    covidApiResults["Pending Resuls"] = resultObj["pending"];
-    covidApiResults["Hospitalized Currently"] = resultObj["hospitalizedCurrently"];
-    covidApiResults["Hospitalized Cumulative"] = resultObj["hospitalizedCumulative"];
-    covidApiResults["Hospitalized Increase"] = resultObj["hospitalizedIncrease"];
-    covidApiResults["Currently in ICU"] = resultObj["inIcuCurrently"];
-    covidApiResults["Cumulative in ICU"] = resultObj["inIcuCumulative"];
-    covidApiResults["Currently on Ventilator"] = resultObj["onVentilatorCurrently"];
-    covidApiResults["Cumulative on Ventilator"] = resultObj["onVentilatorCumulative"];
-    covidApiResults["Recovered Cases"] = resultObj["recovered"];
-    covidApiResults["# of Deaths"] = resultObj["death"];
-    covidApiResults["Increase in Deaths"] = resultObj["deathIncrease"];
+    // use toLocaleString() to add commas every 3 digits
+    covidApiResults["Positive Cases"] = resultObj["positive"].toLocaleString();
+    covidApiResults["Positive Cases Increase"] = resultObj["positiveIncrease"].toLocaleString();
+    covidApiResults["Negative Cases"] = resultObj["negative"].toLocaleString();
+    covidApiResults["Negative Cases Increase"] = resultObj["negativeIncrease"].toLocaleString();
+    covidApiResults["Pending Resuls"] = resultObj["pending"].toLocaleString();
+    covidApiResults["Hospitalized Currently"] = resultObj["hospitalizedCurrently"].toLocaleString();
+    covidApiResults["Hospitalized Cumulative"] = resultObj["hospitalizedCumulative"].toLocaleString();
+    covidApiResults["Hospitalized Increase"] = resultObj["hospitalizedIncrease"].toLocaleString();
+    covidApiResults["Currently in ICU"] = resultObj["inIcuCurrently"].toLocaleString();
+    covidApiResults["Cumulative in ICU"] = resultObj["inIcuCumulative"].toLocaleString();
+    covidApiResults["Currently on Ventilator"] = resultObj["onVentilatorCurrently"].toLocaleString();
+    covidApiResults["Cumulative on Ventilator"] = resultObj["onVentilatorCumulative"].toLocaleString();
+    covidApiResults["Recovered Cases"] = resultObj["recovered"].toLocaleString();
+    covidApiResults["# of Deaths"] = resultObj["death"].toLocaleString();
+    covidApiResults["Increase in Deaths"] = resultObj["deathIncrease"].toLocaleString();
 
     this.apiResultObj = covidApiResults;
     
     this.isApiDataAvailable = true;
+  }
+
+  formatDate(date) {
+    // date (retrieved from api) is in the following format: year month day (with no spaces)
+    date = date.toString();
+    
+    // use substring to get year, month and day from the date string
+    // ranges: year (0, 4) | month (4, 6) | day (6, 8)
+    let year = date.substring(0, 4);
+    let month = date.substring(4, 6);
+    let day = date.substring(6, 8);
+
+    // assign values to formattedString variable and return
+    let formattedDate = month + " / " + day + " / " + year;
+    console.log('formatted date ', formattedDate);
+
+    return formattedDate;
   }
 
 }
